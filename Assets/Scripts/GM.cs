3217e083d394413db70a8ce0ae48ff17f33dcb04
bhison <catbat04@gmail.com>
2016-05-15 17:49:11 +0100

    Beginning of tutorial
    http://catlikecoding.com/unity/tutorials/maze/

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,39 @@
+﻿//Maze game made for http://catlikecoding.com/unity/tutorials/maze/
+
+using UnityEngine;
+using System.Collections;
+
+public class GM : MonoBehaviour {
+
+	public Maze mazePrefab;
+
+	private Maze mazeInstance;
+
+	// Use this for initialization
+	void Start () 
+	{
+		BeginGame ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			RestartGame ();
+		}
+	}
+
+	void BeginGame()
+	{
+		mazeInstance = Instantiate (mazePrefab) as Maze;
+		StartCoroutine(mazeInstance.Generate ());
+	}
+
+	void RestartGame()
+	{
+		StopAllCoroutines ();
+		Destroy (mazeInstance.gameObject);
+		BeginGame ();
+	}
+}
